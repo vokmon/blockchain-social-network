df97319b0abf294f376bf7d276d8ee5878bfebc5
Arnon Ruangthanawes <arnon.ruangthanawes@integrationworks.co.th>
2020-11-22 20:47:20 +0700

    Create a web page with reactjs to connect to the smart contract

---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Identicon from 'identicon.js';
+
+const Navbar = ({ account }) => {
+  return (
+    <nav className='navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow'>
+      <a
+        className='navbar-brand col-sm-3 col-md-2 mr-0'
+        href='http://www.dappuniversity.com/bootcamp'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        Dapp University
+      </a>
+      <ul className='navbar-nav px-3'>
+        <li className='nav-item text-nowrap d-none d-sm-none d-sm-block'>
+          <small className='text-secondary'>
+            <small id='account'>{account}</small>
+          </small>
+          {account && (
+            <img
+              alt='Account icon'
+              className='ml-2'
+              width='30'
+              height='30'
+              src={`data:image/png;base64,${
+                new Identicon(account, 30).toString()
+              }`}
+            />
+          )}
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
