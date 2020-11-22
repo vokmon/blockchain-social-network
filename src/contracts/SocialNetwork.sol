4552e1403937babf0e1770c0e1cfc89f8c71ab1b
Arnon Ruangthanawes <arnon.ruangthanawes@integrationworks.co.th>
2020-11-22 19:54:56 +0700

    Complete the smart contract

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -2,8 +2,70 @@ pragma solidity ^0.5.0;
 
 contract SocialNetwork {
   string public name;
+  uint public postCount = 0;
+
+  mapping(uint => Post) public posts;
+
+  struct Post {
+    uint id;
+    string content;
+    uint tipAmount;
+    address payable author;
+  }
+
+  event PostCreated(
+    uint id,
+    string content,
+    uint tipAmount,
+    address payable author
+  );
+
+  event PostTipped(
+    uint id,
+    string content,
+    uint tipAmount,
+    address payable author
+  );
 
   constructor() public {
     name = "Dapp University Social Network";
+  }
+
+  function createPost(string memory _content) public {
+    // Require valid content
+    require(bytes(_content).length > 0, '');
+
+    // Increment the post count
+    postCount ++;
+
+    // Create the post and save
+    posts[postCount] = Post(postCount, _content, 0, msg.sender);
+
+    // Trigger event
+    emit PostCreated(postCount, _content, 0, msg.sender);
+  }
+
+  function tipPost(uint _id) public payable {
+    // Make sure the id is valid
+    require(_id > 0 && _id <= postCount, "Invalid post id");
+
+    // Fetch the post
+    Post memory _post = posts[_id];
+
+    // Fetch the author
+    address payable _author = _post.author;
+
+    // Pay the author by sendint them Ether
+    address(_author).transfer(msg.value);
+
+    // Increment the tip amount
+    // 1 Ether = 1000000000000000000 Wei
+    _post.tipAmount = _post.tipAmount +  msg.value;
+
+    // Update the post
+    posts[_id] = _post;
+
+    // Trigger an event
+    emit PostTipped(_id, _post.content, _post.tipAmount, _post.author);
   }
 }
